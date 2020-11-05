4de8c68913fc70f41f36d8eab1889d08a22e9db2
Yann Vigara <yvigara@gmail.com>
2020-11-05 11:03:37 +0000

    Automatically download the ssh-key-agent if not present

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,6 +16,7 @@ data "ignition_systemd_unit" "ssh-key-agent" {
       uri     = var.uri
       groups  = "${join(",", var.groups)}"
       version = var.agent_version
+      source  = "https://github.com/utilitywarehouse/ssh-key-agent/releases/download/${var.agent_version}/ssh-key-agent_${var.agent_version}_linux_amd64"
     }
   )
 }
