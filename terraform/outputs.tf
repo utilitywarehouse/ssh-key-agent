c7e682c3eca4e638dcba3b32d55257747761a14f
George Angel <github-7574494a@infosecproject.org>
2020-12-04 14:54:13 +0000

    Clean up resource names

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,14 +2,14 @@ output "unit" {
   value = data.ignition_systemd_unit.ssh-key-agent.rendered
 }
 
-output "download-unit" {
-  value = data.ignition_systemd_unit.ssh-key-agent-download.rendered
-}
-
 output "file" {
   value = data.ignition_file.ssh-key-agent.rendered
 }
 
 output "docker_unit" {
   value = data.ignition_systemd_unit.ssh_key_agent_docker.rendered
+}
+
+output "download_unit" {
+  value = data.ignition_systemd_unit.ssh-key-agent-download.rendered
 }
