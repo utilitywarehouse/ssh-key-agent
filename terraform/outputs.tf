bdff6a5839e25285d2935015054d490f23a541b4
George Angel <github-7574494a@infosecproject.org>
2020-12-07 10:41:14 +0000

    Fix resource name refs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,9 @@
 output "unit" {
-  value = data.ignition_systemd_unit.ssh-key-agent.rendered
+  value = data.ignition_systemd_unit.ssh_key_agent.rendered
 }
 
 output "file" {
-  value = data.ignition_file.ssh-key-agent.rendered
+  value = data.ignition_file.ssh_key_agent.rendered
 }
 
 output "docker_unit" {
@@ -11,5 +11,5 @@ output "docker_unit" {
 }
 
 output "download_unit" {
-  value = data.ignition_systemd_unit.ssh-key-agent-download.rendered
+  value = data.ignition_systemd_unit.ssh_key_agent_download.rendered
 }
